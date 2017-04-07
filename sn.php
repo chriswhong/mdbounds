d638a4bfe6f85919d57c39ac0b2c5340e8af54ab
Chris Whong <chris@whong.org>
2017-04-06 23:30:55 -0400

    first commit

---
+++ sn.php
@@ -0,0 +1,18 @@
+<!--Newproject-->
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>N45HT sec</title>
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet"> 
+</head>
+<body bgcolor="black">
+<table width="100%" height="100%">
+<td align="center">
+<b><font color="yellow" face='iceland' size="10">siluman.nanas</font></b><br>
+<font color='white' face='Space mono' size='4'>- N 4 5 H T s e c -</font>
+<p class="style14" align="center"> <font color="#ffffff" face="courier new" size="3"> Gretzz :</font><marquee scrolldelay="20" scrollamount="2" direction="left" behavior="scroll" width="50%">
+<font color="#ff0000" face="Tahoma" size="3"> | IdaRahma | Kill Me | Broken Angel | ShinChan | ./Mr.Blank007 | SCYTHE404_LOL | PETR03X | R3xYn-01` | J4NC0K | FRU_403 | kasian | MRY | siluman.nanas | /mr.Andro0TFL | Triple A | Noob_Orb | BOCAH KRIMINAL | r007@unkn0wn | MrX404 | Misters$ | AbsTrax | Inisial-C | user98 | JerukID27 | TinamaT_TeaM | Mr. S3PUL0H | MR.U13 | Dark_Oracle | Mr. x0x | Mr.Cr4bs | SexyGirl | BangDebbz | VrCy | R3xYn-01`| B4RC0D3 // 
+</td>
+</table>
+<embed src="https://www.youtube.com/v/Q9JGZTAFxHM&autoplay=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed>
+</body>
